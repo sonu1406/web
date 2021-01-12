2168ce702868493772e541ca3bb47130ae9929a8
ashreeeth1406 <ppranees@cisco.com>
2021-01-12 21:11:13 +0530

    jobs updated

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Job
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    jobs=Job.objects
+    return render(request,'./jobs/home.html', {"jobs":jobs})
